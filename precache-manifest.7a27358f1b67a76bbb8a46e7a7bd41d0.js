3c3531c0b578941231b798117e170532e955f74f
Daniela Alves <daniela.a@conversionkings.com.au>
2020-10-02 12:53:18 +1000

    Updates

---
+++ precache-manifest.7a27358f1b67a76bbb8a46e7a7bd41d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f00d3fa6d7a257f2937b6de7dd74d0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b65ee35a98c129fda745",
+    "url": "/monsters-rolodex/static/css/main.ac46ffc3.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65ee35a98c129fda745",
+    "url": "/monsters-rolodex/static/js/main.3b7f249d.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
