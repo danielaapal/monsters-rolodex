2c90da7905171ebe32568b00a5f54d8a1ec3de4b
Daniela Alves <daniela.a@conversionkings.com.au>
2020-10-02 12:27:14 +1000

    backgroudn and title

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends Component {
         return (
             <div className="App">
                 <div className="content">
+                    <h1>Monsters Rolodex</h1>
                     <SearchBox 
                         placeholder='Search Monsters'
                         handleChange={this.handleChange}    
